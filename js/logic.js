af64d518c69557a59bf92ead303ac6f8317b63ad
ezjava@gmail.com <ezjava@gmail.com>
2016-11-13 14:26:19 -0400

    add new images

--- js/logic.js
+++ js/logic.js
@@ -1,3 +1,11 @@
   $(document).ready(function(){
       $('.slider').slider({full_width: true});
-    });
+ 
+ 	$('a[rel="relativeanchor"]').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $.attr(this, 'href') ).offset().top
+	    }, 500);
+	    return false;
+	}); 
+
+ });
